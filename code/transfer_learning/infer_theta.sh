09089048010350d044b02929d58831916fe6514a
Zhi Wen <zhiwen120@gmail.com>
2021-03-30 13:01:18 -0400

    update infer_theta.sh

--- code/transfer_learning/infer_theta.sh
+++ code/transfer_learning/infer_theta.sh
@@ -2,6 +2,6 @@
 cuda=$1
 
 CUDA_VISIBLE_DEVICES=${cuda} python infer_theta.py \
-    --save_dir /home/mcb/users/zwen8/data/covid/results/WHO_910_transfer_harm_0 \
-    --data_dir /home/mcb/users/zwen8/data/covid/WHO_910_transfer_harm_0/min_df_10 \
-    --model_dir /home/mcb/users/zwen8/data/covid/results/cnpi/Aylien_july_cdc_harm/old/lstm/seed_0/01-05-15-09/
+    --save_dir "directory to save results" \
+    --data_dir "directory containing processed WHO data" \
+    --model_dir "the trained MixMedia model"
