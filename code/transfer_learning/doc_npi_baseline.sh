267a12e041abe66ccd4a6668f6bd1e6f3f0a59ea
Zhi Wen <zhiwen120@gmail.com>
2021-03-30 12:02:26 -0400

    update doc_npi_baseline.sh

--- code/transfer_learning/doc_npi_baseline.sh
+++ code/transfer_learning/doc_npi_baseline.sh
@@ -2,10 +2,10 @@
 cuda=$1
 
 CUDA_VISIBLE_DEVICES=${cuda} python doc_npi_baseline.py \
-    --data_dir /home/mcb/users/zwen8/data/covid/WHO_910_transfer_harm_0/min_df_10 \
-    --save_dir /home/mcb/users/zwen8/data/covid/results/WHO_910_transfer_harm_0 \
-    --epochs 10 \
-    --num_seeds 100 \
-    --lr 1e-4 \
-    --no_logger \
-    # --save_ckpt \
+    --data_dir "directory contianing bow" \
+    --save_dir "directory to save output" \
+    --epochs "# of epochs" \
+    --num_seeds "# of random runs" \
+    --lr "learning rate" \
+    # --save_ckpt \ # save output to save_dir
+    # --linear \    # use linear classifier
